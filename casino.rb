dd96d49a53f81d4bb5b8e095590b2d7be627d372
steph medlin <stephanieanndavis13@gmail.com>
2021-11-25 11:55:05 -0500

    updated player to have wallet working

--- casino.rb
+++ casino.rb
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 
 require_relative "wallet.rb"
 require_relative "slot_machine.rb"
@@ -76,3 +77,79 @@ class Casino
       end
     
     end
+=======
+require_relative 'player.rb'
+require_relative 'blackjack.rb'
+require_relative 'slots.rb'
+require_relative 'rockpaperscissors.rb'
+# require_relative 'wallet.rb'
+
+
+class Casino
+  def initialize
+    @player = Player.new
+    menu
+  end  
+
+  def play_game
+    begin
+      puts "What would you like to do?"
+      puts "1. Blackjack"
+      puts "2. Slots"
+      puts "3. Rock, Paper, Scissors"
+      puts "4. View Wallet"
+      puts "5. Never mind, I don't want to play"
+      response = gets.strip.to_i
+      raise "Bad input, try again" unless response > 0 && response < 6
+      case response
+      when 1
+        #Blackjack.new(@player)
+        Blackjack.new(@player)
+      when 2
+        Slots.new(@player)
+      when 3
+        Rockpaperscissors.new(@player)
+      when 4
+        puts "You have $#{@player.wallet}"  
+      when 5
+        puts "Have a nice day!"
+        exit
+      else
+        raise  
+      end
+    rescue StandardError => e
+      puts e
+      retry
+    end
+  end
+
+
+  def menu 
+    begin
+      puts "Welcome! Please make a selection."
+      puts "1. Play a Game"
+      puts "2. View Wallet"
+      puts "3. Exit"
+      response = gets.strip.to_i
+      raise "Bad input, try again" unless response > 0 && response < 4
+      case response
+      when 1  
+        play_game
+      when 2
+        puts "You have $#{@player.wallet}"  
+      when 3
+          puts "See ya next time!"
+          exit
+      else
+        raise
+      end      
+    rescue StandardError => e
+      puts e
+      retry
+    end
+  end
+end
+
+
+Casino.new
+>>>>>>> 3e67a03 (updated player to have wallet working)
--- player.rb
+++ player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require_relative "wallet.rb"
 
 
@@ -12,3 +13,40 @@ class Player
 end    
     
   
+=======
+
+class Player
+  attr_accessor :name, :wallet #might not be necessary
+  def initialize
+    puts "What is your name?"
+    @name = gets.strip
+    @name = name
+    puts "How much money do you have?" #add
+    @wallet = gets.strip.to_i #could also be money
+    # @wallet = Wallet.new(@money)
+    puts "You have $#{@wallet}."
+    # @wallet == @money
+  end
+
+  def current_balance
+    return @wallet
+  end
+
+  def validate_money(money)
+    if money < 0 && (@wallet - money) >= 0
+      true
+    else
+      false
+    end
+  end
+
+  def add_money(money)
+    @wallet += money
+  end
+
+  def subtract_money(money)
+    @wallet -= money
+  end
+
+end   
+>>>>>>> 3e67a03 (updated player to have wallet working)
